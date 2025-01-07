aaea427d8ac753bb69d8daf372aad67fd3784ed4
elvisaghaulor <aghaulorelvis@gmail.com>
2025-01-07 12:39:48 +0100

    number one solution

---
+++ NumberOneArraySolution/NumberOneArraySolution/ArrayHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOneArraySolution
+{
+    internal class ArrayHandler
+    {
+        public static void CalculateTotalScore(int[] inputArray)
+        {
+            if (inputArray.Length == 0)
+            {
+                Console.WriteLine("array is empty");
+            }
+
+            int totalScore = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int currentItem = inputArray[i];
+
+                if (currentItem % 2 == 0)
+                {
+                    totalScore += 1;
+                    if (currentItem == 8)
+                    {
+                        totalScore += 5;
+                    }
+                }
+                else
+                {
+                    totalScore += 3;
+                }
+            }
+
+            Console.WriteLine($"Total: {totalScore}");
+        }
+    }
+}
--- NumberOneArraySolution/NumberOneArraySolution/Program.cs
+++ NumberOneArraySolution/NumberOneArraySolution/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Kindly modify the integer array below to the values you want
+            //int[] intArray = { 1,2,3,4,5 };
+            //int[] intArray = { 15,25,35};
+            int[] intArray = { 8, 8 };
+            ArrayHandler.CalculateTotalScore(intArray);
+            Console.ReadKey();
         }
     }
 }
