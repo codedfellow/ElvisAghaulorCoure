a2603f326326ba2b3e1b9465cba5113cca1d433b
elvisaghaulor <aghaulorelvis@gmail.com>
2025-01-07 11:58:01 +0100

    initial commit

---
+++ NumberTwoWebAPI/NumberTwoWebAPI/Controllers/CountryController.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NumberTwoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+        public CountryController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet]
+        [Route("get-country-details-with-phone-number")]
+        public async Task<IActionResult> GetCountryDetailsWithPhoneNumber([FromQuery]string phoneNumber)
+        {
+            return Ok("yet to be implemented");
+        }
+    }
+}
